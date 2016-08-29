693fbd7ba81c0312eac618a2f96a024ade8d32dd
Alsciende <bertolini.cedric@me.com>
2016-08-29 12:17:40 +0200

    use proper alert

--- src/AppBundle/Resources/public/js/nrdb.data.js
+++ src/AppBundle/Resources/public/js/nrdb.data.js
@@ -133,7 +133,7 @@ NRDB.data = {};
 				 */
 				if($('.site-title').length === 0) {
 					var message = "A new version of the data is available. Click <a href=\"javascript:window.location.reload(true)\">here</a> to reload your page.";
-					$('<div class="bg-info">').html(message).insertAfter($('nav'));
+					$('<div class="alert alert-info">').html(message).insertAfter($('nav'));
 				}
 			}
 			
@@ -143,7 +143,7 @@ NRDB.data = {};
 			 */
 			if(!_.every(dataLoaded)) {
 				var message = "Unable to load the data. Click <a href=\"javascript:window.location.reload(true)\">here</a> to reload your page.";
-				$('<div class="bg-info">').html(message).insertAfter($('nav'));
+				$('<div class="alert alert-info">').html(message).insertAfter($('nav'));
 				return;
 			} else {
 				data.release();
