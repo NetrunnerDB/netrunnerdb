a98c5de7d19ddccffbd8b8a2fa863de15832be48
Alsciende <bertolini.cedric@me.com>
2016-12-29 14:18:39 +0100

    fix issue with tooltips appearing off-screen

--- src/AppBundle/Resources/public/js/nrdb.tip.js
+++ src/AppBundle/Resources/public/js/nrdb.tip.js
@@ -1,38 +1,42 @@
+/* global NRDB */
+
 (function (tip, $) {
 
     var hide_event = 'mouseout',
-        prevent_all = false;
+            prevent_all = false;
 
     tip.prevent = function (event) {
         prevent_all = true;
     };
-    
+
     tip.display = function (event) {
-        if (prevent_all || $(this).hasClass('no-popup')) {
+        var $this = $(this);
+
+        if(prevent_all || $this.hasClass('no-popup')) {
             return;
         }
-            
-        var code = $(this).data('index')
-                || $(this).closest('.card-container').data('index')
-                || ($(this).attr('href') && $(this).attr('href').replace(
+
+        var code = $this.data('index')
+                || $this.closest('.card-container').data('index')
+                || ($this.attr('href') && $this.attr('href').replace(
                         /.*\/card\/(\d\d\d\d\d).*/,
                         "$1"));
         var card = NRDB.data.cards.findById(code);
-        if (!card)
+        if(!card)
             return;
         var type = '<p class="card-info">' + NRDB.format.type(card) + '</p>';
         var influence = '';
-        for (var i = 0; i < card.faction_cost; i++)
+        for(var i = 0; i < card.faction_cost; i++)
             influence += "●";
-        if (card.strength != null)
+        if(card.strength !== null)
             type += '<p>Strength <b>' + card.strength + '</b></p>';
         var image_svg = '';
-        if ($('#nrdb_svg_hex').length && typeof InstallTrigger === 'undefined') {
+        if($('#nrdb_svg_hex').length && typeof InstallTrigger === 'undefined') {
             // no hexagon for Firefox, bad boy!
             image_svg = '<div class="card-image card-image-' + card.side_code + '-' + card.type_code + '"' + (card.imageUrl ? ' style="background-image:url(' + card.imageUrl + ')"' : '')
                     + '><svg width="103px" height="90px" viewBox="0 0 677 601" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><mask id="mask"><use xlink:href="#rect" style="fill:white" /><use xlink:href="#hex" style="fill:black"/></mask><use xlink:href="#rect" mask="url(#mask)"/><use xlink:href="#hex" style="stroke:black;fill:none;stroke-width:15" /></svg></div>';
         }
-        $(this).qtip(
+        $this.qtip(
                 {
                     content: {
                         text: image_svg
@@ -48,10 +52,13 @@
                     },
                     position: {
                         my: 'left center',
-                        at: 'right center'
+                        at: 'right center',
+                        viewport: $(document.body),
+                        adjust: {
+                            method: 'flip'
+                        }
                     },
                     show: {
-                        event: event.type,
                         ready: true,
                         solo: true
                     },
@@ -62,14 +69,14 @@
     };
 
     tip.set_hide_event = function set_hide_event(opt_hide_event) {
-        if (opt_hide_event == 'mouseout' || opt_hide_event == 'unfocus') {
+        if(opt_hide_event === 'mouseout' || opt_hide_event === 'unfocus') {
             hide_event = opt_hide_event;
         }
-    }
+    };
 
     $(document).on('data.app', function () {
         $('body').on({
-            touchstart: tip.prevent,
+            touchstart: tip.prevent
         });
         $('body').on({
             mouseover: tip.display
