b340f4f686879427624a2f0bfe39106af9eb584a
Andrew Vandever <andrew.vandever@gmail.com>
2017-10-08 16:56:02 -0700

    Add "latest pack at time of writing" to card reviews

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -255,8 +255,8 @@ class SearchController extends Controller
 			'full' => 20,
 			'images' => 20,
 			'short' => 1000,
-                        'rulings' => 200,
-		    'zoom' => 1,
+			'rulings' => 200,
+			'zoom' => 1,
 		];
 		
 		$synonyms = [
@@ -315,12 +315,26 @@ class SearchController extends Controller
 				}
 				$cardinfo['available'] = $availability[$pack->getCode()];
 				if($view == "zoom") {
+					$allsetsdata = $this->get('cards_data')->allsetsdata();
 				    $cardinfo['reviews'] = $this->get('cards_data')->get_reviews($card);
-                                    $cardinfo['rulings'] = $this->get('cards_data')->get_rulings($card);
+					$dbh = $this->get('doctrine')->getConnection();
+					foreach ($cardinfo['reviews'] as $key => $review) {
+						// Trouvez le dernier pack publié au moment de la création de l'avis
+						$rows = $dbh->executeQuery(
+							"SELECT name
+							FROM pack
+							WHERE date_release > ?
+							ORDER by date_release DESC
+							LIMIT 1",
+							array($review['date_creation']->format('U'))
+							)->fetchAll(\PDO::FETCH_ASSOC);
+						$cardinfo['reviews'][$key]['latestpack'] = $rows[0]['name'];
+					}
+					$cardinfo['rulings'] = $this->get('cards_data')->get_rulings($card);
 				}
-                                if($view == "rulings") {
-                                    $cardinfo['rulings'] = $this->get('cards_data')->get_rulings($card);
-                                }
+				if($view == "rulings") {
+					$cardinfo['rulings'] = $this->get('cards_data')->get_rulings($card);
+				}
 				$cards[] = $cardinfo;
 			}
 
