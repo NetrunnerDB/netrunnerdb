f690fdb5d6b62251613ec63ea7ebde0dab1f49c8
Cédric Bertolini <c.bertolini@wakeonweb.com>
2017-12-04 18:02:32 +0100

    symfony 3.4.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,7 +29,6 @@ class AppKernel extends Kernel
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
         }
