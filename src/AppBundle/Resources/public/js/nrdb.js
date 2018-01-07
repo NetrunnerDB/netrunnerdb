3037f19441a683aa86a2abab7a22813295a61b46
Alsciende <bertolini.cedric@me.com>
2018-01-07 15:14:57 +0100

    improve unicorne style

--- src/AppBundle/Resources/public/js/nrdb.js
+++ src/AppBundle/Resources/public/js/nrdb.js
@@ -416,8 +416,8 @@ function update_deck(options) {
             additional_info = '(<span class="small icon icon-' + card.pack.cycle.code + '"></span> ' + card.position + ') ' + alert_number_of_sets + influence;
         }
 
-        var mwlCard = get_mwl_modified_card(card)
-        var unicorn = mwlCard.is_restricted ? "🦄 " : ""
+        var mwlCard = get_mwl_modified_card(card);
+        var unicorn = mwlCard.is_restricted ? '<span style="display:inline-block;width:1.5em;">🦄</span> ' : '';
         var item = $('<div>' + card.indeck + 'x <a href="' + Routing.generate('cards_zoom', { card_code: card.code }) + '" class="card" data-toggle="modal" data-remote="false" data-target="#cardModal" data-index="' + card.code + '">' + card.title + '</a> ' + unicorn + additional_info + '</div>');
         item.appendTo($('#deck-content .deck-' + criteria));
 
