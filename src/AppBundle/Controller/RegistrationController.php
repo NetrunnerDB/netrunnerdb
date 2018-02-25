276896fdd546ea7958df2e069e1f1c22e3da59c0
Alsciende <bertolini.cedric@me.com>
2018-02-25 21:00:33 +0100

    return of the RegistrationController

---
+++ src/AppBundle/Controller/RegistrationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Description of RegistrationController
+ *
+ * @author Alsciende <alsciende@icloud.com>
+ */
+class RegistrationController extends BaseController
+{
+    /**
+     * Receive the confirmation token from user email provider, login the user
+     */
+    public function confirmAction(Request $request, $token)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+        $user = $userManager->findUserByConfirmationToken($token);
+        if (null === $user) {
+            // User with token not found. Do whatever you want here
+            return new RedirectResponse($this->container->get('router')->generate('fos_user_security_login'));
+        } else {
+            // Token found. Letting the FOSUserBundle's action handle the confirmation
+            return parent::confirmAction($request, $token);
+        }
+    }
+}
