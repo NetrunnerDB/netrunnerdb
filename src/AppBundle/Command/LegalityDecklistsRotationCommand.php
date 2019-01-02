1b11ebc64350b0ee2d21302785db932ad533ad19
Noah Bogart <noah.bogart@ebth.com>
2019-01-02 11:31:36 -0500

    Multi-line string instead of string concats

--- src/AppBundle/Command/LegalityDecklistsRotationCommand.php
+++ src/AppBundle/Command/LegalityDecklistsRotationCommand.php
@@ -32,23 +32,23 @@ class LegalityDecklistsRotationCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $sql = "UPDATE decklist d"
-            . "JOIN decklistslot s ON d.id = s.decklist_id"
-            . "JOIN card c ON s.card_id = c.id"
-            . "JOIN pack p ON c.pack_id = p.id"
-            . "JOIN cycle y ON p.cycle_id = y.id"
-            . "SET d.is_legal = 1"
-            . "WHERE d.is_legal = 0"
-            . "AND y.rotated = 1"
-            . "AND EXISTS ("
-            . "    SELECT *"
-            . "    FROM card nc"
-            . "    JOIN pack np ON nc.pack_id = np.id"
-            . "    JOIN cycle ny ON np.cycle_id = ny.id"
-            . "    WHERE nc.id <> c.id"
-            . "    AND nc.title = c.title"
-            . "    AND np.id >= p.id"
-            . "    AND ny.rotated = 0);";
+        $sql = "UPDATE decklist d
+            JOIN decklistslot s ON d.id = s.decklist_id
+            JOIN card c ON s.card_id = c.id
+            JOIN pack p ON c.pack_id = p.id
+            JOIN cycle y ON p.cycle_id = y.id
+            SET d.is_legal = 1
+            WHERE d.is_legal = 0
+            AND y.rotated = 1
+            AND EXISTS (
+                SELECT *
+                FROM card nc
+                JOIN pack np ON nc.pack_id = np.id
+                JOIN cycle ny ON np.cycle_id = ny.id
+                WHERE nc.id <> c.id
+                AND nc.title = c.title
+                AND np.id >= p.id
+                AND ny.rotated = 0);";
 
         $this->entityManager->getConnection()->executeQuery($sql);
 
