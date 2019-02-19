057f7dba226cf8f97f24a08a9ea84ea5db85a84a
Jason Gessner <jason@multiply.org>
2019-02-19 15:05:08 +0000

    Make sort function more readable.

--- web/js/nrdb.js
+++ web/js/nrdb.js
@@ -307,7 +307,16 @@ function update_deck(options) {
     for (var colnum = 0; colnum < displayDescription.length; colnum++) {
         var rows = displayDescription[colnum];
         // Don't rely on the rows being put into displayDescription in order.
-        rows.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
+	// Explicitly sort them by their provided ID.
+        rows.sort((a,b) => {
+          if (a.id < b.id) {
+            return -1;
+          }
+          if (a.id > b.id) {
+            return 1;
+          }
+          return 0;
+	});
 
         var div = $('<div>').addClass('col-sm-' + cols_size).appendTo($('#deck-content'));
         for (var rownum = 0; rownum < rows.length; rownum++) {
