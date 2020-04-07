8512e4687576bf4e061606c03f336064ae72615c
Jason Gessner <jason@multiply.org>
2020-04-07 12:21:03 -0500

    Add search operator to specify rotations.

--- src/AppBundle/Service/RotationService.php
+++ src/AppBundle/Service/RotationService.php
@@ -22,6 +22,22 @@ class RotationService
         $this->entityManager = $entityManager;
     }
 
+    /**
+     * Returns the first entry of the descending sorted rotations
+     * @return Rotation current Rotation
+     */
+    public function findCurrentRotation()
+    {
+        $rotation = $this->entityManager->getRepository(Rotation::class)->findOneBy([], ['dateStart' => 'DESC']);
+
+        // There should always be a rotation available.
+        if (!$rotation) {
+            throw new \Exception("No current rotation found", 1);
+        }
+
+        return $rotation;
+    }
+
     public function findCompatibleRotation(Decklist $decklist)
     {
         $rotations = $this->entityManager->getRepository(Rotation::class)->findBy([], ['dateStart' => 'DESC']);
@@ -51,4 +67,14 @@ class RotationService
             return $cycle->getCode();
         }, $rotation->getCycles()->toArray()))) === 0;
     }
+
+    /**
+     * @param string $code
+     * @return Rotation rotation with the matching code or null if not found.
+     */
+    public function findRotationByCode(string $code)
+    {
+        return $this->entityManager->getRepository(Rotation::class)->findOneBy(['code' => $code]);
+    }
+
 }
