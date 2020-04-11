da840f5bc90f8de21bfbdcd616252a80b342b862
Jason Gessner <jason@multiply.org>
2020-04-11 12:55:33 -0500

    Lazily load most images that aren't expected to be on screen at page load.
    This was suggested by the lighthouse audit and should help a bit with
    page responsiveness.

--- web/js/deck.js
+++ web/js/deck.js
@@ -54,9 +54,9 @@ Promise.all([NRDB.data.promise, NRDB.settings.promise]).then(function() {
 	factions.forEach(function(faction) {
 		var label = $('<label class="btn btn-default btn-sm" data-code="' + faction.code
 				+ '" title="'+faction.name+'"><input type="checkbox" name="' + faction.code
-				+ '"><img src="'
+				+ '"><img data-src="'
 				+ Url_FactionImage.replace('xxx', faction.code)
-				+ '" style="height:12px" alt="'+faction.code+'"></label>');
+				+ '" style="height:12px" class="lazyload" alt="'+faction.code+'"></label>');
 		label.tooltip({container: 'body'});
 		$('#faction_code').append(label);
 	});
@@ -78,8 +78,8 @@ Promise.all([NRDB.data.promise, NRDB.settings.promise]).then(function() {
 	types.forEach(function(type) {
 		var label = $('<label class="btn btn-default btn-sm" data-code="'
 				+ type.code + '" title="'+type.name+'"><input type="checkbox" name="' + type.code
-				+ '"><img src="' + Url_TypeImage.replace('xxx', type.code)
-				+ '" style="height:12px" alt="'+type.code+'"></label>');
+				+ '"><img data-src="' + Url_TypeImage.replace('xxx', type.code)
+				+ '" style="height:12px" class="lazyload" alt="'+type.code+'"></label>');
 		label.tooltip({container: 'body'});
 		$('#type_code').append(label);
 	});
@@ -702,9 +702,9 @@ function build_div(record) {
 	switch (Number(NRDB.settings.getItem('display-columns'))) {
 	case 1:
 
-		var imgsrc = record.faction_code.substr(0,7) === "neutral" ? "" : '<img src="'
+		var imgsrc = record.faction_code.substr(0,7) === "neutral" ? "" : '<img data-src="'
 				+ Url_FactionImage.replace('xxx', record.faction_code)
-				+ '" alt="'+record.faction.name+'">';
+				+ '" class="lazyload" alt="'+record.faction.name+'">';
 		div = $('<tr class="card-container" data-index="'
 				+ record.code
 				+ '"><td><div class="btn-group" data-toggle="buttons">'
@@ -714,8 +714,8 @@ function build_div(record) {
 				+ '" data-target="#cardModal" data-remote="false" data-toggle="modal">'
 				+ record.title + '</a> '+get_influence_penalty_icons(record)+'</td><td class="influence influence-' + record.faction_code
 				+ '">' + influ + '</td><td class="type" title="' + record.type.name
-				+ '"><img src="/images/types/'
-				+ record.type_code + '.png" alt="'+record.type.name+'">'
+				+ '"><img data-src="/images/types/'
+				+ record.type_code + '.png" class="lazyload" alt="'+record.type.name+'">'
 				+ '</td><td class="faction" title="' + record.faction.name + '">'
 				+ imgsrc + '</td></tr>');
 		break;
@@ -726,7 +726,7 @@ function build_div(record) {
 				+ record.code
 				+ '">'
 				+ '<div class="media"><div class="media-left">'
-				+ '<img class="media-object" src="'+record.imageUrl+'" alt="'+record.title+'">'
+				+ '<img class="media-object lazyload" data-src="'+record.imageUrl+'" alt="'+record.title+'">'
 				+ '</div><div class="media-body">'
 				+ '    <h4 class="media-heading"><a class="card" href="'
 				+ Routing.generate('cards_zoom', {card_code:record.code})
@@ -743,7 +743,7 @@ function build_div(record) {
 				+ record.code
 				+ '">'
 				+ '<div class="media"><div class="media-left">'
-				+ '<img class="media-object" src="'+record.imageUrl+'" alt="'+record.title+'">'
+				+ '<img class="media-object lazyload" data-src="'+record.imageUrl+'" alt="'+record.title+'">'
 				+ '</div><div class="media-body">'
 				+ '    <h5 class="media-heading"><a class="card" href="'
 				+ Routing.generate('cards_zoom', {card_code:record.code})
