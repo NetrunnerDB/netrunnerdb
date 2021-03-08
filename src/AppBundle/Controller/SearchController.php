185aae868b4447f54f530e6d4481329ff4139f19
Jason Gessner <jason@multiply.org>
2021-03-07 19:00:34 -0600

    Fix variable scoping issue triggered when no cards are found.
    This is swallowed in prod and shows a "Your query didn't match any
    card." message, but in dev, there is a stack trace.

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -405,6 +405,8 @@ class SearchController extends Controller
 
         $cardsData->validateConditions($conditions);
 
+        $card = null;
+
         // reconstruction of the correct search string for display
         $q = $cardsData->buildQueryFromConditions($conditions);
         $rows = $cardsData->get_search_rows($conditions, $sort, $locale);
@@ -439,7 +441,6 @@ class SearchController extends Controller
             }
             $last = $first + $nb_per_page;
 
-            $card = null;
             $versions = null;
 
             if ($view == "zoom") {
