0efe60c2f1766dbd5318819cdc274c3a42a4dcc6
Jason Gessner <jason@multiply.org>
2021-03-30 18:58:28 -0500

    In progress banlist page.

--- src/AppBundle/Controller/BanlistsController.php
+++ src/AppBundle/Controller/BanlistsController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Legality;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BanlistsController extends Controller
@@ -9,8 +11,24 @@ class BanlistsController extends Controller
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getAction()
+    public function getAction(EntityManagerInterface $entityManager)
     {
-        return $this->render('/Banlists/banlists.html.twig');
+        $mwls = $entityManager->getRepository('AppBundle:Mwl')->findBy([], ['dateStart' => 'DESC']);
+		$banlists = array();
+        foreach ($mwls as $mwl) {
+			$x = array();
+			$x['name'] = $mwl->getName();
+			$x['active'] = $mwl->getActive();
+			$x['code'] = $mwl->getCode();
+			$x['start_date'] = $mwl->getDateStart();
+			$x['cards'] = $mwl->getCards();
+			$x['mwl_object_delete'] = $mwl;
+			$banlists[] = $x;
+        }
+
+        return $this->render('/Banlists/banlists.html.twig', [
+         'pagetitle'  => "Ban Lists",
+         'banlists'   => $banlists,
+        ]);
     }
 }
