ec69360a7a89b86c4ad179d169f7753147a74cc4
distributive <github@ameliewd.com>
2022-01-16 22:17:58 +0000

    Fixes and formatting

--- web/js/initbuild.js
+++ web/js/initbuild.js
@@ -17,79 +17,71 @@ $(function() {
         }
     });
     function updateFormat(format) {
-      $('.identity').each(function(id, i) {
-        // All
-        if (format == 'all') {
-          $(this).removeClass('hidden-format');
-          return;
-        }
-        // Other formats
-        let visible = true;
-        // Startup
-        if (format == 'startup') {
-          visible = ['df', 'urbp', 'ur', 'sg', 'su21'].some(p => $(this).hasClass('pack-' + p));
-        }
-        // Standard
-        else if (format == 'standard') {
-          visible = !($(this).hasClass('banned') || $(this).hasClass('rotated'));
-        }
-        // Eternal
-        else if (format == 'eternal') {
-          visible = true
-        }
-        // Draft
-        if ($(this).hasClass('pack-draft')) {
-          visible = format == 'draft';
-        }
-        else if (format == 'draft') {
-          visible = false
-        }
-        // Other (neutral Gateway IDs and the multiplayer NAPD ID)
-        if (['24001', '30076', '30077'].includes($(this).attr('data-code'))) {
-          visible = format == 'other';
-        }
-        else if (format == 'other') {
-          visible = false
-        }
-        // Apply effect
-        if (visible)
-          $(this).removeClass('hidden-format');
-        else
-          $(this).addClass('hidden-format');
-      });
+        $('.identity').each(function(id, i) {
+            // All
+            if (format == 'all') {
+                $(this).removeClass('hidden-format');
+                return;
+            }
+            // Other formats
+            let visible = true;
+            // Startup
+            if (format == 'startup') {
+                visible = ['df', 'urbp', 'ur', 'sg', 'su21'].some(p => $(this).hasClass('pack-' + p));
+            }
+            // Standard
+            else if (format == 'standard') {
+                visible = !($(this).hasClass('banned') || $(this).hasClass('rotated'));
+            }
+            // Eternal
+            else if (format == 'eternal') {
+                visible = true
+            }
+            // Draft
+            if ($(this).hasClass('pack-draft')) {
+                visible = format == 'draft';
+            }
+            else if (format == 'draft') {
+                visible = false
+            }
+            // Other (neutral Gateway IDs and the multiplayer NAPD ID)
+            if (['24001', '30076', '30077'].includes($(this).attr('data-code'))) {
+                visible = format == 'other';
+            }
+            else if (format == 'other') {
+                visible = false
+            }
+            // Apply effect
+            if (visible)
+                $(this).removeClass('hidden-format');
+            else
+                $(this).addClass('hidden-format');
+        });
     }
     function updateMisc() {
-      let faction = $('#faction_filter').val();
-      let search = $('#title-filter').val().toLowerCase();
-      $('.identity').each(function(id, i) {
-        if (faction != 'all' && !$(this).hasClass('faction-' + faction) || !$(this).find('.name').html().toLowerCase().includes(search)) {
-          $(this).addClass('hidden-misc');
-          return;
-        }
-        $(this).removeClass('hidden-misc');
-      });
-    }
-    function applyUpdate() {
-      $('.identity:not(.hidden-format, .hidden-misc)').show();
-      $('.identity.hidden-format, .identity.hidden-misc').hide();
+        let faction = $('#faction-filter').val();
+        let search = $('#title-filter').val().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        $('.identity').each(function(id, i) {
+            if (faction != 'all' && !$(this).hasClass('faction-' + faction) || !$(this).find('.name').html().toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(search)) {
+                $(this).addClass('hidden-misc');
+                return;
+            }
+            $(this).removeClass('hidden-misc');
+        });
     }
     // Filter on format selected
-    $('#format-filter > a').on('click', function(event) {
-      updateFormat($(this).attr('value'));
-      applyUpdate()
-      event.preventDefault();
+    $('.option-format').on('click', function(event) {
+        updateFormat($(this).attr('value'));
+        event.preventDefault();
     });
     // Filter on faction selected
-    $('#faction_filter').change(function() {
-      updateMisc();
-      applyUpdate()
+    $('#faction-filter').change(function() {
+        updateMisc();
     });
     // Filter on search updated
     $('#title-filter').on('input', function() {
-      updateMisc();
-      applyUpdate()
+        updateMisc();
     });
     // Filter on page refresh
     updateMisc();
-    applyUpdate()
 });
