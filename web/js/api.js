9abc1ffb38d0402b83ec438453d4e393ac40b53f
distributive <github@ameliewd.com>
2022-09-24 19:44:00 +0100

    General improvements to banlist page

--- web/js/api.js
+++ web/js/api.js
@@ -1,7 +1,18 @@
-// Fetches all cards from the v2 API
-async function fetchCards() {
-  data = [];
-  json = await fetch(`${v3_api_url}/api/v3/public/cards`).then(data => data.json());
+// Fetches the json from an api url
+async function fetchJson(url) {
+  return await fetch(url).then(data => data.json());
+}
+
+// Fetches the data field from a url (not accounting for pagination)
+async function fetchData(url) {
+  return await fetchJson(url).then(json => json.data);
+}
+
+// Fetches all cards from the v2 API (accounting for pagination)
+// Returns a list of cards
+async function fetchCards(flags='') {
+  const data = [];
+  let json = await fetch(`${v3_api_url}/api/v3/public/cards/${flags}`).then(data => data.json());
   data.push(...json.data);
   while ('next' in json.links) {
     json = await fetch(json.links.next).then(data => data.json());
@@ -10,21 +21,19 @@ async function fetchCards() {
   return data;
 }
 
-// Remove all cards with any of the given subtypes from a list of cards
-function removeCardsWithSubtypes(cards, subtypes) {
-  if (!subtypes || subtypes.length == 0)
-    return cards;
-  return cards.filter(c => subtypes.some(s => !(c.attributes.display_subtypes && c.attributes.display_subtypes.toLowerCase().split(' - ').includes(s))));
+// Removes currents from a list of cards
+// Assumes the cards have their list of subtypes exposed
+function removeCurrents(cards) {
+  return cards.filter(c => !c.relationships.card_subtypes.data.map(d => d.id).includes('current'));
 }
 
-// Searches a list of restrictions for the one with the given ID
-function getRestrictionById(restrictions, id) {
-  for (i = 0; i < restrictions.length; i++) {
-    if (restrictions[i].id == id) {
-      return restrictions[i];
-    }
-  }
-  return null;
+// Turns a json object containing a list of objects with an ID into a map from IDs to those objects
+function makeIdMap(json) {
+  const out = new Map();
+  json.data.forEach(d => {
+    out.set(d.id, d);
+  });
+  return out;
 }
 
 // Filters a list of cards for the one with the given ID
@@ -42,8 +51,8 @@ function getCardsByIdFromObj(cards, obj) {
 
 // Splits a list of cards into corp cards and runner cards
 function splitBySide(cards) {
-  corp = [];
-  runner = [];
+  const corp = [];
+  const runner = [];
   cards.forEach (card => {
     if (card.attributes.side_id == 'corp')
       corp.push(card);
@@ -54,6 +63,6 @@ function splitBySide(cards) {
 }
 
 // Generates a link to a card by creating a link to its most recent printing
-function cardToLink(card) {
+function cardToLatestPrintingLink(card) {
   return Routing.generate('cards_zoom', {card_code:card.attributes.latest_printing_id});
 }
